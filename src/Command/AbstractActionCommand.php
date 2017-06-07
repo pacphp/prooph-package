b036a045d13a78b6637a6ea4b42a7665ee457663
iampersistent <develop@zestic.com>
2017-06-07 03:15:12 -0700

    add getAggregateId() method, change field to method

--- src/Command/AbstractActionCommand.php
+++ src/Command/AbstractActionCommand.php
@@ -12,17 +12,20 @@ use Ramsey\Uuid\Uuid;
 
 abstract class AbstractActionCommand extends Command implements PayloadConstructable
 {
-    const ID_FIELD = 'id';
-
     use PayloadTrait;
+
+    public function getAggregateId(): string
+    {
+        return $this->payload[static::rootIdFieldName()];
+    }
 
     public static function fromPayload($payload): Command
     {
-        if (empty($payload[static::ID_FIELD])) {
+        if (empty($payload[static::rootIdFieldName()]) && static::idCreationAllowed()) {
             $uuid = Uuid::uuid4();
-            $payload += [static::ID_FIELD => $uuid->getHex()];
+            $payload += [static::rootIdFieldName() => $uuid->getHex()];
         } else {
-            $uuid = Uuid::fromString($payload[static::ID_FIELD]);
+            $uuid = Uuid::fromString($payload[static::rootIdFieldName()]);
         }
 
         $createdAt = new DateTimeImmutable('now', new DateTimeZone('UTC'));
@@ -38,4 +41,6 @@ abstract class AbstractActionCommand extends Command implements PayloadConstruct
         return static::fromArray($message);
     }
 
+    abstract protected static function idCreationAllowed(): bool;
+    abstract protected static function rootIdFieldName(): string;
 }
